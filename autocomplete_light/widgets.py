dac9b87fc74ffd3368fec10e2044b209b7d181ea
jpic <jamespic@gmail.com>
2012-05-09 21:52:06 +0200

    Documented AutocompleteWidget

--- autocomplete_light/widgets.py
+++ autocomplete_light/widgets.py
@@ -9,10 +9,44 @@ from .channel import *
 __all__ = ['AutocompleteWidget']
 
 class AutocompleteWidget(forms.SelectMultiple):
-    class Media:
-        js = ('autocomplete_light/deck.js',)
+    """
+    Widget suitable for ModelChoiceField and ModelMultipleChoiceField.
+    """
 
     def __init__(self, channel_name, *args, **kwargs):
+        """
+        Decorates SelectMultiple constructor 
+
+        Arguments:
+        channel_name -- the name of the channel that this widget should use.
+
+        Keyword arguments are passed to javascript via data attributes of the
+        autocomplete wrapper element:
+
+        max_items
+            The number of items that this autocomplete allows. If set to 0,
+            then it allows any number of selected items like a multiple select,
+            well suited for ManyToMany relations or any kind of
+            ModelMultipleChoiceField. If set to 3 for example, then it will
+            only allow 3 selected items. It should be set to 1 if the widget is
+            for a ModelChoiceField or ForeignKey, in that case it would be like
+            a normal select. Default is 0.
+        
+        min_characters
+            The minimum number of characters before the autocomplete box shows
+            up. If set to 2 for example, then the autocomplete box will show up
+            when the input receives the second character, for example 'ae'. If
+            set to 0, then the autocomplete box will show up as soon as the
+            input is focused, even if it's empty, behaving like a normal
+            select. Default is 0.
+
+        bootstrap
+            The name of the bootstrap kind. By default, deck.js will only
+            initialize decks for wrappers that have data-bootstrap="normal". If
+            you want to implement your own bootstrapping logic in javascript,
+            then you set bootstrap to anything that is not "normal". By
+            default, its value is copied from channel.bootstrap.
+        """
         self.channel_name = channel_name
         
         from autocomplete_light import registry
@@ -25,6 +59,18 @@ class AutocompleteWidget(forms.SelectMultiple):
         super(AutocompleteWidget, self).__init__(*args, **kwargs)
 
     def render(self, name, value, attrs=None):
+        """
+        Render the autocomplete widget.
+
+        It will try two templates, like django admin:
+        - autocomplete_light/channelname/widget.html
+        - autocomplete_light/widget.html
+
+        Note that it will not pass 'value' to the template, because 'value'
+        might be a list of model ids in the case of ModelMultipleChoiceField,
+        or a model id in the case of ModelChoiceField. To keep things simple,
+        it will just pass a list, 'values', to the template context.
+        """
         final_attrs = self.build_attrs(attrs)
         self.html_id = final_attrs.pop('id', name)
 
@@ -55,15 +101,15 @@ class AutocompleteWidget(forms.SelectMultiple):
     # we might want to split up in two widgets for that ... is it necessary ?
     # apparently not yet, but maybe at next django release
     def value_from_datadict(self, data, files, name):
+        """Route to Select if max_items is 1, else route to SelectMultiple"""
         if self.max_items == 1:
             return forms.Select.value_from_datadict(self, data, files, name)
         else:
             return forms.SelectMultiple.value_from_datadict(self, data, files, name)
 
     def _has_changed(self, initial, data):
+        """Route to Select if max_items is 1, else route to SelectMultiple"""
         if self.max_items == 1:
             return forms.Select._has_changed(self, initial, data)
         else:
             return forms.SelectMultiple._has_changed(self, initial, data)
-
-
