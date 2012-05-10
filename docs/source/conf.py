3503a18e48cdff548bbe15788eadb75a48649685
jpic <jamespic@gmail.com>
2012-05-10 09:23:32 +0200

    Try a new theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,6 +21,8 @@ sys.path.insert(0, os.path.abspath('../../../../lib/python2.7/site-packages/'))
 from django.conf import settings
 settings.configure()
 
+html_style = None
+
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
