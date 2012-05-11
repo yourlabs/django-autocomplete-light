4165873f083246bbaee6a2d4ac284af40280c495
jpic <jamespic@gmail.com>
2012-05-11 09:09:50 +0200

    Added a simple test

---
+++ autocomplete_light/tests.py
@@ -0,0 +1,13 @@
+import unittest
+
+from autocomplete_light.templatetags import autocomplete_light_tags
+
+class StaticTagTestCase(unittest.TestCase):
+    def test_output(self):
+        expected = ''.join([
+            '<script src="/static/autocomplete_light/autocomplete.js" type="text/javascript"></script>',
+            '<script src="/static/autocomplete_light/deck.js" type="text/javascript"></script>',
+            '<link rel="stylesheet" type="text/css" href="/static/autocomplete_light/style.css" />',
+        ])
+        output = autocomplete_light_tags.autocomplete_light_static()
+        self.assertEqual(output, expected)
