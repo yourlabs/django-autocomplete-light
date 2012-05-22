1d3ad095f757246bda844758740f15133aae5e58
James Pic <jamespic@gmail.com>
2012-05-22 09:37:40 +0200

    Override search_name by default, just for demonstration purposes

--- test_project/project_specific/autocomplete_light_registry.py
+++ test_project/project_specific/autocomplete_light_registry.py
@@ -2,6 +2,6 @@ import autocomplete_light
 
 from models import Contact
 
-autocomplete_light.register(Contact)
+autocomplete_light.register(Contact, search_name='name')
 
 from generic_channel_example import *
