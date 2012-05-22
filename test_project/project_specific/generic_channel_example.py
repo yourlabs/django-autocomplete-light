05cb698d45ce4e33e2f4bfdc38f9633083a284a7
James Pic <jamespic@gmail.com>
2012-05-22 10:11:11 +0200

    Implement query_filter for MyGenericChannel, because it should search by something other than search_name in the case of Address

--- test_project/project_specific/generic_channel_example.py
+++ test_project/project_specific/generic_channel_example.py
@@ -15,4 +15,15 @@ class MyGenericChannel(autocomplete_light.GenericChannelBase):
         elif results.model == Contact:
             return results.order_by('name')
 
+    def query_filter(self, results):
+        q = self.request.GET.get('q', None)
+
+        if q:
+            if results.model == Address:
+                results = results.filter(street__icontains=q)
+            elif results.model == Contact:
+                results = results.filter(name__icontains=q)
+
+        return results
+
 autocomplete_light.register(MyGenericChannel)
