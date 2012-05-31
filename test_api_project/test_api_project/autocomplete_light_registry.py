fccf3df85eb79ea7f270e454f5bb9eda162985f9
James Pic <jamespic@gmail.com>
2012-05-31 15:28:02 +0200

    Update example to match current register signature, avoids subclassing

--- test_api_project/test_api_project/autocomplete_light_registry.py
+++ test_api_project/test_api_project/autocomplete_light_registry.py
@@ -3,11 +3,7 @@ import autocomplete_light
 from cities_light.contrib.autocomplete_light_restframework import RemoteCountryChannel, RemoteCityChannel
 from cities_light.models import City, Country
 
-class RemoteCountryChannel(RemoteCountryChannel):
-    source_url = 'http://localhost:8000/cities_light/country/'
-
-class RemoteCityChannel(RemoteCityChannel):
-    source_url = 'http://localhost:8000/cities_light/city/'
-
-autocomplete_light.register(Country, RemoteCountryChannel)
-autocomplete_light.register(City, RemoteCityChannel)
+autocomplete_light.register(Country, RemoteCountryChannel,
+    source_url = 'http://localhost:8000/cities_light/country/')
+autocomplete_light.register(City, RemoteCityChannel,
+    source_url = 'http://localhost:8000/cities_light/city/')
