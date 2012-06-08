e296eb3556e206e92abe37b3318dd7403fc96b12
James Pic <jamespic@gmail.com>
2012-06-08 16:53:43 +0200

    Sync cities in more countries, added region filter

--- test_project/project_specific/models.py
+++ test_project/project_specific/models.py
@@ -8,10 +8,17 @@ from genericm2m.models import RelatedObjectsDescriptor
 import cities_light
 
 
+COUNTRIES = ('FR', 'US', 'BE', 'UK', 'ES', 'PT', 'DE', 'NL')
+
 def filter_city_import(sender, items, **kwargs):
-    if items[8] not in ('FR', 'US', 'BE'):
+    if items[8] not in COUNTRIES:
         raise cities_light.InvalidItems()
 cities_light.signals.city_items_pre_import.connect(filter_city_import)
+
+def filter_region_import(sender, items, **kwargs):
+    if items[0].split('.')[0] not in COUNTRIES:
+        raise cities_light.InvalidItems()
+cities_light.signals.region_items_pre_import.connect(filter_region_import)
 
 class Contact(models.Model):
     name = models.CharField(max_length=100)
