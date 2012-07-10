0ed8d5013d56c7fcbd8d2c17600e36c552666e08
James Pic <jamespic@gmail.com>
2012-07-10 20:00:24 +0200

    Version bump 1.0.3

--- autocomplete_light/registry.py
+++ autocomplete_light/registry.py
@@ -125,7 +125,7 @@ class AutocompleteRegistry(dict):
                 model._meta.get_field('name')
             except:
                 raise Exception(u'Add search_fields kwargs to .register(%s)'
-                    % model.__class__.__name__)
+                    % model.__name__)
             else:
                 kwargs['search_fields'] = ['name']
 
--- test_remote_project/remote_autocomplete/autocomplete_light_registry.py
+++ test_remote_project/remote_autocomplete/autocomplete_light_registry.py
@@ -1,8 +1,13 @@
-from cities_light.contrib.autocompletes import CityRestAutocomplete
-from cities_light.models import City
+from cities_light.contrib.autocompletes import *
 
 import autocomplete_light
 
+
+autocomplete_light.register(Country, CountryRestAutocomplete,
+    source_url='http://localhost:8000/cities_light/country/')
+
+autocomplete_light.register(Region, RegionRestAutocomplete,
+    source_url='http://localhost:8000/cities_light/region/')
 
 autocomplete_light.register(City, CityRestAutocomplete,
     source_url='http://localhost:8000/cities_light/city/')
