22332e878eee090aceacba14c7a72854bdc4908c
James Pic <jamespic@gmail.com>
2012-07-12 20:32:54 +0200

    yourlabs.Autocomplete.fetch() fetches more.
    Before, it would only refresh if the value of the input has changed.
    However, the data attribute of the autocomplete may have changed even if
    the input value has changed. This is now fixed.

--- autocomplete_light/static/autocomplete_light/autocomplete.js
+++ autocomplete_light/static/autocomplete_light/autocomplete.js
@@ -456,19 +456,14 @@ window.yourlabs.Autocomplete = function (input) {
 
     // Proxy fetch(), with some sanity checks.
     this.refresh = function() {
-        var newValue = this.input.val();
+        // Set the new current value.
+        this.value = this.input.val();
 
         // If the input contains the placehold then abort.
-        if (newValue == this.placeholder) return false;
+        if (this.value == this.placeholder) return false;
 
         // If the input doesn't contain enought characters then abort.
-        if (newValue.length < this.minimumCharacters) return false;
-
-        // If the input hasn't changed value then abort.
-        if (newValue == this.value) return false;
-
-        // Set the new current value.
-        this.value = newValue;
+        if (this.value.length < this.minimumCharacters) return false;
 
         // All clear, continue on refreshing the autocomplete.
         this.fetch();
