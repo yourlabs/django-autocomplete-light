2d4f32091205b44f495d3715c9377f79fe463027
James Pic <jamespic@gmail.com>
2012-07-13 10:40:19 +0200

    Restored sanity check before fetching a new autocomplete, fixed keyboard navigation and selenium tests

--- autocomplete_light/static/autocomplete_light/autocomplete.js
+++ autocomplete_light/static/autocomplete_light/autocomplete.js
@@ -214,6 +214,12 @@ window.yourlabs.Autocomplete = function (input) {
     this.xhr = false;
 
     /*
+    fetch() keeps a copy of the data sent to the server in this attribute. This
+    avoids double fetching the same autocomplete.
+     */
+    this.lastData = {};
+
+    /*
     Rather than directly setting up the autocomplete (DOM events etc ...) in
     the constructor, setup is done in this method. This allows to:
 
@@ -479,6 +485,22 @@ window.yourlabs.Autocomplete = function (input) {
 
         // Add the current value to the data dict.
         this.data[this.queryVariable] = this.value;
+
+        // Ensure that this request is different from the previous one
+        changed = false;
+        for(var key in this.data) {
+            if (!key in this.lastData || this.data[key] != this.lastData[key]) {
+                changed = true;
+                break;
+            }
+        }
+
+        if (!changed) return true;
+
+        this.lastData = {};
+        for(var key in this.data) {
+            this.lastData[key] = this.data[key];
+        }
 
         // Make an asynchronous GET request to this.url.
         this.xhr = $.ajax(this.url, {
