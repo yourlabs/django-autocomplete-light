67a25e527c6200e5c1f0a0c9c555e415ae33c901
jpic <jamespic@gmail.com>
2012-11-09 15:50:46 +0100

    Provide autocomplete_light.FixedModelForm

--- autocomplete_light/__init__.py
+++ autocomplete_light/__init__.py
@@ -4,6 +4,6 @@ Provide tools to enable nice autocompletes in your Django project.
 from .registry import AutocompleteRegistry, registry, register, autodiscover
 from .autocomplete import *
 from .widgets import ChoiceWidget, MultipleChoiceWidget, TextWidget
-from .forms import get_widgets_dict, modelform_factory
+from .forms import get_widgets_dict, modelform_factory, FixedModelForm
 from .generic import GenericModelForm, GenericModelChoiceField
 from .views import CreateView
