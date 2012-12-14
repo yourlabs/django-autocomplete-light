b697481ebd1e65b55f92d16327e358a8491dfe2d
jpic <jamespic@gmail.com>
2012-12-14 10:55:36 +0100

    Version bump: 1.1.8

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ else:
 
 setup(
     name='django-autocomplete-light',
-    version='1.1.7',
+    version='1.1.8',
     description='Simple alternative to django-ajax-selects',
     author='James Pic',
     author_email='jamespic@gmail.com',
