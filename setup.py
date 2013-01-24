4f9ecffc70e09c267d5c138d1d45a23a9fa9e68a
jpic <jamespic@gmail.com>
2013-01-24 03:52:11 +0100

    Version bump 1.1.11

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ else:
 
 setup(
     name='django-autocomplete-light',
-    version='1.1.10',
+    version='1.1.11',
     description='Fresh autocompletes for Django',
     author='James Pic',
     author_email='jamespic@gmail.com',
