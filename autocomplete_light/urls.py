f997cf7036a376502fc2c4160b71d9dfc593112c
Patrick Taylor <patricktaylor@mac.com>
2013-02-12 07:59:17 -0400

    PEP8 clean up

--- autocomplete_light/urls.py
+++ autocomplete_light/urls.py
@@ -5,16 +5,16 @@ autocomplete_light_autocomplete
     Given a 'autocomplete' argument with the name of the autocomplete, this url
     routes to AutocompleteView.
 """
+
 try:
     from django.conf.urls.defaults import patterns, url
 except ImportError:
     from django.conf.urls import patterns, url
 
-
-
 from django.views.generic.base import TemplateView
 
 from views import AutocompleteView, RegistryView
+
 
 urlpatterns = patterns('',
     url(r'^(?P<autocomplete>[-\w]+)/$',
