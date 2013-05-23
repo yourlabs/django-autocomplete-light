a9bde90564f73e86115596b6ae663aa177f07bb9
jpic <jamespic@gmail.com>
2013-05-23 13:12:47 +0200

    Version bump 1.2.0

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ else:
 
 setup(
     name='django-autocomplete-light',
-    version='1.1.31',
+    version='1.2.0',
     description='Fresh autocompletes for Django',
     author='James Pic',
     author_email='jamespic@gmail.com',
