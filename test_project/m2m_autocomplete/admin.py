63f560a46db6fbb035e92bcf557c6729ea1fa4a0
jpic <jamespic@gmail.com>
2013-07-30 11:15:40 +0200

    Added m2m inline example

--- test_project/m2m_autocomplete/admin.py
+++ test_project/m2m_autocomplete/admin.py
@@ -5,6 +5,14 @@ import autocomplete_light
 from models import VisitedCities
 
 
+class VisitedCitiesInline(admin.TabularInline):
+    form = autocomplete_light.modelform_factory(VisitedCities)
+    model = VisitedCities
+
+
 class VisitedCitiesAdmin(admin.ModelAdmin):
     form = autocomplete_light.modelform_factory(VisitedCities)
+    inlines = [VisitedCitiesInline]
+
+
 admin.site.register(VisitedCities, VisitedCitiesAdmin)
--- test_project/m2m_autocomplete/models.py
+++ test_project/m2m_autocomplete/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class VisitedCities(models.Model):
     cities = models.ManyToManyField('cities_light.City')
+    parent = models.ForeignKey('self', null=True, blank=True)
 
     class Meta:
         verbose_name_plural = 'visited cities'
