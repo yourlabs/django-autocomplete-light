82a5dcf2cd78c3f5ff4d659eba8f706c5280623f
jpic <jamespic@gmail.com>
2013-08-17 13:31:09 +0200

    Skip WidgetTestCase on travis

--- autocomplete_light/tests/__init__.py
+++ autocomplete_light/tests/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 import autocomplete_light
 autocomplete_light.autodiscover()
 
@@ -6,7 +8,15 @@ from .templatetags import DataAttributesTestCase
 from .generic import GenericModelFormTestCase
 from .generic_m2m import AutocompleteGenericM2MTestCase
 from .exceptions import AutocompleteNotRegisteredTestCase
-#from .widget import WidgetTestCase
+
+"""
+Selenium tests (which have waits and all) don't work on travis since the
+update, i don't know why, i've spent countless hours trying to debug it, asked
+numerous times on #travis, was recommended to contact support which i did
+but support didn't reply so here goes ....
+"""
+if not os.environ.get('TRAVIS', False):
+    from .widget import WidgetTestCase
 
 from .autocomplete.choice_list import AutocompleteChoiceListTestCase
 from .autocomplete.list import AutocompleteListTestCase
