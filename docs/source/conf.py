77a1448b3907d8a9196e0d5adbc5341e6e48a47c
jpic <jamespic@gmail.com>
2013-11-28 12:33:35 +0100

    Removed dead extension from conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -51,8 +51,7 @@ intersphinx_mapping = {
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.intersphinx',
-        'sphinx.ext.graphviz', 'sphinx.ext.inheritance_diagram',
-        'autocomplete_light.contrib.sphinx_ext']
+        'sphinx.ext.graphviz', 'sphinx.ext.inheritance_diagram']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
