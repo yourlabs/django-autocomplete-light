a06747c1990dedec9862b24132932f6ff07f3e12
jpic <jamespic@gmail.com>
2013-12-11 14:28:29 +0100

    Use the short django version for

--- test.sh
+++ test.sh
@@ -9,14 +9,14 @@ DJANGO_GENERIC_M2M="${DJANGO_GENERIC_M2M:-1}"
 PYTHON_VERSION="${PYTHON_VERSION:-3.3}"
 DJANGO_VERSION="${DJANGO_VERSION:-1.5}"
 
+# Make a unique env path for this configuration
+ENV_PATH="/tmp/autocomplete-light-test-$PYTHON_VERSION-$DJANGO_VERSION-$DJANGO_TAGGIT-$DJANGO_GENERIC_M2M"
+
 # Get real django version
 [ ! -d /tmp/django ] && git clone http://github.com/django/django.git $WORKSPACE/.django
 cd $WORKSPACE/.django
 git fetch --tags
 DJANGO_VERSION=$(git tag -l | grep -E "$DJANGO_VERSION(\.[0-9])?$" | tail -n1)
-
-# Make a unique env path for this configuration
-ENV_PATH="/tmp/autocomplete-light-test-$PYTHON_VERSION-$DJANGO_VERSION-$DJANGO_TAGGIT-$DJANGO_GENERIC_M2M"
 
 # Clean virtualenv if necessary
 [ "$CLEAN_VIRTUALENV" = "1" ] && rm -rf $ENV_PATH
