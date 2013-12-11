5c2ac7219ea632138cdc96b62823b4cdc6586b72
jpic <jamespic@gmail.com>
2013-12-11 17:28:39 +0100

    Typo in varname fix

--- test_project/test_project/settings_postgres.py
+++ test_project/test_project/settings_postgres.py
@@ -6,7 +6,7 @@ from .settings import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.env.get('DATABASE_NAME', 'autocomplete_light_test'),                      # Or path to database file if using sqlite3.
+        'NAME': os.environ.get('DATABASE_NAME', 'autocomplete_light_test'),                      # Or path to database file if using sqlite3.
         'USER': 'postgres',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
