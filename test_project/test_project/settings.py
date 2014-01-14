714d1debf6dea4e0520cca3e1fedc6fe7eb529ec
Sebastian Ramirez <sebastian.rmirz@gmail.com>
2014-01-14 12:59:12 -0800

    Add django-taggit.

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -176,6 +176,7 @@ INSTALLED_APPS = (
     'support_sandino',
     'default_template_autocomplete',
     'ajax_create',
+    'taggit',
 )
 
 # A sample logging configuration. The only tangible logging
