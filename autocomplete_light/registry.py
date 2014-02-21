f32e13ac002b001b3af24e2bfbad2e4fb2759fd9
Igor Mitrenko <netwamp@gmail.com>
2014-02-21 08:40:11 +0400

    Django 1.7 SimpleAdminConfig quick-and-dirty fix

--- autocomplete_light/registry.py
+++ autocomplete_light/registry.py
@@ -229,7 +229,11 @@ def _autodiscover(registry):
     from django.utils.module_loading import module_has_submodule
 
     for app in settings.INSTALLED_APPS:
-        mod = import_module(app)
+        try:
+            mod = import_module(app)
+        except ImportError, e:
+            #not all entries in INSTALLED_APPS are have to be modules
+            continue
         # Attempt to import the app's admin module.
         try:
             before_import_registry = copy.copy(registry)
