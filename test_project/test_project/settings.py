4e2971a7e6def459b187f3dfe1209d1c802f7841
jpic <jamespic@gmail.com>
2014-04-27 12:42:04 +0200

    Update SOUTH_MIGRATIONS_MODULES for taggit.

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -63,6 +63,10 @@ LOGGING = {
     }
 }
 
+SOUTH_MIGRATIONS_MODULES = {
+    'taggit': 'taggit.south_migrations',
+}
+
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
