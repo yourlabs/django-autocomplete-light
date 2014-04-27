1157a15f01938bc6f49f85d96e269d951ad3b213
jpic <jamespic@gmail.com>
2014-04-27 13:13:54 +0200

    Added missing docstring

--- autocomplete_light/urls.py
+++ autocomplete_light/urls.py
@@ -4,6 +4,10 @@ An url to AutocompleteView.
 autocomplete_light_autocomplete
     Given a 'autocomplete' argument with the name of the autocomplete, this url
     routes to AutocompleteView.
+
+autocomplete_light_registry
+    Renders the autocomplete registry, good for debugging, requires being
+    authenticated as superuser.
 """
 
 try:
