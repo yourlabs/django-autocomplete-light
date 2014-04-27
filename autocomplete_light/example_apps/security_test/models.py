395347c02247cbc1f455638a34d1c7b674257d69
jpic <jamespic@gmail.com>
2014-04-27 17:22:18 +0200

    Python2/3 model unicode compat

--- autocomplete_light/example_apps/security_test/models.py
+++ autocomplete_light/example_apps/security_test/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class Item(models.Model):
     name = models.CharField(max_length=100)
     items = models.ManyToManyField('self', blank=True)
     private = models.BooleanField(default=True)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
