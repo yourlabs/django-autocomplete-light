246942299ea0fcf7e2bf4f7b3e1dc639eecfaed8
jpic <jamespic@gmail.com>
2015-04-06 21:07:40 +0200

    Backward compatibility

--- autocomplete_light/__init__.py
+++ autocomplete_light/__init__.py
@@ -1,7 +1,12 @@
 """
 Provide tools to enable nice autocompletes in your Django project.
 """
-from .views import *
-from .forms import *
+from django import VERSION
+
+if VERSION < (1, 7):
+    from .shortcuts import *
+else:
+    from .views import *
+    from .forms import *
 
 default_app_config = 'autocomplete_light.apps.AutocompleteLightConfig'
