1218ef26e707abdbe13d2a4f8d2dcbbdd3f25f9f
jpic <jamespic@gmail.com>
2015-04-06 02:56:13 +0200

    Django 1.9: autocomplete_light.* moves to autocomplete_light.shortcuts

--- autocomplete_light/__init__.py
+++ autocomplete_light/__init__.py
@@ -1,31 +1,9 @@
 """
 Provide tools to enable nice autocompletes in your Django project.
 """
-from .registry import AutocompleteRegistry, registry, register, autodiscover
+from django import VERSION
 
-from .autocomplete import *
-
-from .widgets import WidgetBase, ChoiceWidget, MultipleChoiceWidget, TextWidget
-
-from .contrib.taggit_field import TaggitWidget, TaggitField
-
-from .forms import *
-
-from .fields import *
-
-try:
-    import taggit
-except ImportError:
-    pass
-else:
-    from .contrib.taggit_field import TaggitField, TaggitWidget
-
-from .views import CreateView, RegistryView, AutocompleteView
-from .exceptions import (AutocompleteNotRegistered,
-                         NoGenericAutocompleteRegistered,
-                         AutocompleteArgNotUnderstood,
-                         AutocompleteLightException)
-
-from .settings import *
+if VERSION < (1, 9):
+    from .shortcuts import *
 
 default_app_config = 'autocomplete_light.apps.AutocompleteLightConfig'
---
+++ autocomplete_light/shortcuts.py
@@ -0,0 +1,26 @@
+from .registry import AutocompleteRegistry, registry, register, autodiscover
+
+from .autocomplete import *
+
+from .widgets import WidgetBase, ChoiceWidget, MultipleChoiceWidget, TextWidget
+
+from .contrib.taggit_field import TaggitWidget, TaggitField
+
+from .forms import *
+
+from .fields import *
+
+try:
+    import taggit
+except ImportError:
+    pass
+else:
+    from .contrib.taggit_field import TaggitField, TaggitWidget
+
+from .views import CreateView, RegistryView, AutocompleteView
+from .exceptions import (AutocompleteNotRegistered,
+                         NoGenericAutocompleteRegistered,
+                         AutocompleteArgNotUnderstood,
+                         AutocompleteLightException)
+
+from .settings import *
