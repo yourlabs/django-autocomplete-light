42fd4ef247606155a9282001c9df1c6401a9fcb4
jpic <jamespic@gmail.com>
2015-04-08 12:51:42 +0200

    Maintain BC again in autocomplete_light ns until dj19

--- autocomplete_light/__init__.py
+++ autocomplete_light/__init__.py
@@ -3,7 +3,7 @@ Provide tools to enable nice autocompletes in your Django project.
 """
 from django import VERSION
 
-if VERSION < (1, 7):
+if VERSION < (1, 9):
     from .shortcuts import *  # noqa
 else:
     from .views import *  # noqa
