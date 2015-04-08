fa9e1076253bfc99d99b57e16469a99e56f23482
jpic <jamespic@gmail.com>
2015-04-08 12:52:55 +0200

    Version bump 2.1.0rc2 [ci skip]

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ if 'sdist' in sys.argv:
 
 setup(
     name='django-autocomplete-light',
-    version='2.1.0rc1',
+    version='2.1.0rc2',
     description='Fresh autocompletes for Django',
     author='James Pic',
     author_email='jamespic@gmail.com',
