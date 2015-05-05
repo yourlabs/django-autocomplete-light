bfb719b36b50a08ef52e62d3f3c4872e13bcd133
mhuailin <mhuailin@gmail.com>
2015-05-05 10:03:01 -0700

    Get the primary key name from choices so does not have to be id

--- autocomplete_light/autocomplete/model.py
+++ autocomplete_light/autocomplete/model.py
@@ -67,8 +67,9 @@ class AutocompleteModel(object):
         Order choices using :py:attr:`order_by` option if it is set.
         """
         if self.values:
+            pk_name = choices[0]._meta.pk.name
             # Order in the user selection order when self.values is set.
-            clauses = ' '.join(['WHEN id=%s THEN %s' % (pk, i) for i, pk in
+            clauses = ' '.join(['WHEN %s="%s" THEN %s' % (pk_name, pk, i) for i, pk in
                     enumerate(self.values)])
             ordering = 'CASE %s END' % clauses
             return choices.extra(
