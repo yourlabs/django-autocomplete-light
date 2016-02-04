0d2c7889c78f139ecaf5736c3105cff0a926fa75
jpic <jamespic@gmail.com>
2016-02-05 00:11:49 +0100

    No need to install test user on every app's migration

--- test_project/tests/models.py
+++ test_project/tests/models.py
@@ -3,6 +3,9 @@ from django.db.models.signals import post_migrate
 
 
 def test_user(sender, *args, **kwargs):
+    if sender.name != 'django.contrib.auth':
+        return
+
     user, c = User.objects.get_or_create(username='test')
     user.is_staff = True
     user.is_superuser = True
