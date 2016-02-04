c3ff8ca9841dc700bd2e199171c51801341a1927
Eraldo Energy <eraldo@eraldo.org>
2016-02-04 13:15:17 +0100

    fixed loading of jquery if django version os jquery is not present
    The check if django jquery is available which resulted in a reference error if not in the django admin.
    Plus added a missing statement end: `;`.

--- src/dal/static/autocomplete_light/jquery.init.js
+++ src/dal/static/autocomplete_light/jquery.init.js
@@ -1,8 +1,8 @@
 var yl = yl || {};
 
 if (yl.jQuery === undefined) {
-    if (django.jQuery !== undefined)
-        yl.jQuery = django.jQuery
+    if (typeof django !== 'undefined')
+        yl.jQuery = django.jQuery;
 
     else if ($ !== undefined)
         yl.jQuery = $;
