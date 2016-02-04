26aba7a09f748adf0d68b0840a30f5a33d12ae29
jpic <jamespic@gmail.com>
2016-02-04 23:01:55 +0100

    Fix #575 Prevent Django from setting up jQuery with noConflict

---
+++ src/dal/static/admin/js/jquery.init.js
@@ -0,0 +1,8 @@
+var django = django || {};
+django.jQuery = jQuery
+
+var yl = yl || {};
+yl.jQuery = django.jQuery
+
+// this file also prevents django.jQuery.noConflict
+// there's got to be a better way ...
