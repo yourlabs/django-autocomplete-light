71ab3b83740259b42b70609aa65c7d8856e6c39c
jpic <jamespic@gmail.com>
2016-02-23 22:39:49 +0100

    Fix #601: Wrong url in example view configuration

--- test_project/select2_outside_admin/views.py
+++ test_project/select2_outside_admin/views.py
@@ -9,7 +9,7 @@ class UpdateView(generic.UpdateView):
     model = TestModel
     form_class = TestForm
     template_name = 'select2_outside_admin.html'
-    success_url = reverse_lazy('select2_crispy_forms')
+    success_url = reverse_lazy('select2_outside_admin')
 
     def get_object(self):
         return TestModel.objects.first()
