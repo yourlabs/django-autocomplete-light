e98651b8d58525d44f7cd34b7871dab9de6d7ff2
jpic <jamespic@gmail.com>
2016-03-11 02:24:42 +0100

    Request #635: demo of list autocompletion

---
+++ test_project/select2_list/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+
+
+@python_2_unicode_compatible
+class TestModel(models.Model):
+    name = models.CharField(max_length=200)
+
+    test = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True
+    )
+
+    for_inline = models.ForeignKey(
+        'self',
+        null=True,
+        blank=True,
+        related_name='inline_test_models'
+    )
+
+    def __str__(self):
+        return self.name
