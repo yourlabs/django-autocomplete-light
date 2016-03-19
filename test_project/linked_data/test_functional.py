6f2f40ab6c08993eb087a4913e06777f12c7d1ed
jpic <jamespic@gmail.com>
2016-03-19 11:41:07 +0100

    Prefix support in forward feature
    Django forms may have a prefix, in which case all inputs of this form
    will have the prefix in front of their name. It is used for example to
    have 2 different forms on the same page and for formsets.

--- test_project/linked_data/test_functional.py
+++ test_project/linked_data/test_functional.py
@@ -22,17 +22,24 @@ class AdminLinkedDataTest(Select2Story,
             self.fixtures.install_fixtures(self.model)
 
         self.get(url=self.get_modeladmin_url('add'))
+        self.prefix = ''
 
-    def test_filter_options(self):
-        story = stories.SelectOption(self)
+    def set_owner(self, value):
+        self.browser.execute_script(
+            '$("[name=%s]").val(%s)' % (self.prefix + 'owner', value)
+        )
+
+    def test_filter_options(self, story=None):
+        if story is None:
+            story = stories.SelectOption(self)
+
         story.toggle_autocomplete()
 
         expected = self.model.objects.values_list('name', flat=True)
         self.assertEqual(sorted(expected),
                          sorted(story.get_suggestions_labels()))
 
-        self.browser.execute_script('$("[name=owner]").val(%s)' %
-                                    self.fixtures.test.pk)
+        self.set_owner(self.fixtures.test.pk)
         story.refresh_autocomplete()
 
         expected = self.model.objects.filter(
@@ -40,11 +47,20 @@ class AdminLinkedDataTest(Select2Story,
         self.assertEqual(sorted(expected),
                          sorted(story.get_suggestions_labels()))
 
-        self.browser.execute_script('$("[name=owner]").val(%s)' %
-                                    self.fixtures.other.pk)
+        self.set_owner(self.fixtures.other.pk)
         story.refresh_autocomplete()
 
         expected = self.model.objects.filter(
             owner=self.fixtures.other).values_list('name', flat=True)
         self.assertEqual(sorted(expected),
                          sorted(story.get_suggestions_labels()))
+
+    def test_filter_option_in_first_inline(self):
+        self.prefix = '%s-%s-' % (self.inline_related_name, 0)
+        story = stories.InlineSelectOption(self, inline_number=0)
+        self.test_filter_options(story)
+
+    def test_can_select_option_in_first_extra_inline(self):
+        story = stories.InlineSelectOption(self, inline_number=3)
+        self.prefix = '%s-%s-' % (self.inline_related_name, 3)
+        self.test_filter_options(story)
