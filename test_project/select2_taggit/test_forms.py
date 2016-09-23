56e7f0ba776a9893b91569a42c14d9c0da5f4206
Fabio C. Barrioneuvo da Luz <bnafta@gmail.com>
2016-09-23 11:53:43 -0300

    use 'data-autocomplete-light-language' insteadof 'data-language' and add js code
    and qa

--- test_project/select2_taggit/test_forms.py
+++ test_project/select2_taggit/test_forms.py
@@ -66,6 +66,7 @@ class TagSelect2TestMixin(object):
                 attrs={
                     'data-autocomplete-light-function': 'select2',
                     'data-autocomplete-light-url': reverse(self.url_name),
+                    'data-autocomplete-light-language': 'en-US',
                     'data-tags': ',',
                     'id': 'id_test',
                 }
