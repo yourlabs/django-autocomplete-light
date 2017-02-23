b2ecb3dadb580262aecee6aa00f847910415b45e
Italo Maia <italo.maia@gmail.com>
2017-02-23 07:45:24 -0300

    Fixed integration with django js i18n (#802)
    * Fixed integration with django js i18n
    
    https://docs.djangoproject.com/en/1.10/topics/i18n/translation/#internationalization-in-javascript-code
    DAL breaks with i18n
    
    * Adding Italo Maia to authors

--- src/dal/static/autocomplete_light/jquery.init.js
+++ src/dal/static/autocomplete_light/jquery.init.js
@@ -1,7 +1,7 @@
 var yl = yl || {};
 
 if (yl.jQuery === undefined) {
-    if (typeof django !== 'undefined')
+    if ((typeof django !== 'undefined') && (typeof django.jQuery !== 'undefined'))
         yl.jQuery = django.jQuery;
 
     else if (typeof $ !== 'undefined')
