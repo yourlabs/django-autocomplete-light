9ece485cd0a75a2fed608d74ec5bcf330fafffe2
Marek Jędrzejewski <mjedrzejewski92@gmail.com>
2017-04-25 13:17:16 +0200

    Add checkbox handling in forwards

--- test_project/rename_forward/forms.py
+++ test_project/rename_forward/forms.py
@@ -6,13 +6,18 @@ from .models import TModel
 
 
 class TForm(forms.ModelForm):
+    checkbox = forms.BooleanField(label='checkbox',
+                                  required=False,
+                                  initial=False)
+
     class Meta:
         model = TModel
-        fields = ('name', 'owner', 'test')
+        fields = ('name', 'checkbox', 'owner', 'test')
         widgets = {
             'test': autocomplete.ModelSelect2(
                 url='linked_data_rf',
-                forward=(forward.Field(src="owner", dst="possessor"),
+                forward=(forward.Field(src="checkbox"),
+                         forward.Field(src="owner", dst="possessor"),
                          forward.Const(val=42, dst="secret"))
             )
         }
