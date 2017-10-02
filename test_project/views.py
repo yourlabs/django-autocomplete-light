da2eb71abdcdf96fb52b2944fb5848f04b99cab2
jpic <jamespic@gmail.com>
2017-10-02 12:38:23 +0200

    Add loginview to test users which are not staff

--- test_project/views.py
+++ test_project/views.py
@@ -1,5 +1,10 @@
+from django.contrib.auth.views import LoginView
 from django.views import generic
 
 
 class IndexView(generic.TemplateView):
     template_name = 'base.html'
+
+
+class LoginView(LoginView):
+    template_name = 'login.html'
