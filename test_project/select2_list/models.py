57c7d50e9a858a23631c0a33c696e7011bee1fad
jpic <jamespic@gmail.com>
2017-10-02 03:16:56 +0200

    Django 2.0 support

--- test_project/select2_list/models.py
+++ test_project/select2_list/models.py
@@ -14,6 +14,7 @@ class TModel(models.Model):
 
     for_inline = models.ForeignKey(
         'self',
+        models.CASCADE,
         null=True,
         blank=True,
         related_name='inline_test_models'
