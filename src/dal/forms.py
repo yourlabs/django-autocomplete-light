f43536c4431ade0d0b002a04401c877a2e6470f5
Irving Kcam <me@ikcam.com>
2017-12-25 11:48:16 -0500

    Add: FutureForm Django2.0 support

--- src/dal/forms.py
+++ src/dal/forms.py
@@ -123,6 +123,11 @@ class FutureModelForm(forms.ModelForm):
         # Note that for historical reasons we want to include also
         # virtual_fields here. (GenericRelation was previously a fake
         # m2m field).
+        try:  # Django >= 2.0
+            private_fields = opts.private_fields
+        except AttributeError:  # Django < 2.0
+            private_fields = opts.virtual_fields
+
         for f in chain(opts.many_to_many, opts.virtual_fields):
             # Added to give the form field a chance to do the work
             if f.name in handled:
