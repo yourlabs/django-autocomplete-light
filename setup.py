43b3d88d5adeaf7088da22cfed5fc120219aab05
jpic <jamespic@gmail.com>
2017-12-31 16:30:20 +0100

    Revert "Select2 url mutable"
    This reverts commit 5b37f8661cf5d534766f9df19d73b3e92bc94ee5.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name='django-autocomplete-light',
-    version='3.3.0-rc1',
+    version='3.3.0-rc2',
     description='Fresh autocompletes for Django',
     author='James Pic',
     author_email='jamespic@gmail.com',
