645de1c87bfa96910a9dac6c5a2d5141217768d0
jpic <jamespic@gmail.com>
2018-03-10 01:12:21 +0100

    Npm support, build importing select2 raises traceback
    $ webpack
    Hash: ab64bbc6be8e2c1c5cbd
    Version: webpack 3.11.0
    Time: 3996ms
          Asset    Size  Chunks                    Chunk Names
        dist.js  437 kB       0  [emitted]  [big]  main
    dist.js.map  850 kB       0  [emitted]         main
      [90] (webpack)/buildin/global.js 823 bytes {0} [built]
     [125] multi babel-polyfill ./src/dal/js/index.js 40 bytes {0} [built]
     [325] (webpack)/buildin/module.js 521 bytes {0} [built]
     [329] ./src/dal/js/index.js 499 bytes {0} [built]
        + 328 hidden modules
    
    ERROR in /home/jpic/node_modules/jquery/dist/jquery.js
    Module build failed: Error: Couldn't find preset "stage-2" relative to directory "/home/jpic/node_modules/jquery/dist"
        at /home/jpic/src/django-autocomplete-light/node_modules/babel-core/lib/transformation/file/options/option-manager.js:293:19
        at Array.map (<anonymous>)
        at OptionManager.resolvePresets (/home/jpic/src/django-autocomplete-light/node_modules/babel-core/lib/transformation/file/options/option-manager.js:275:20)
        at OptionManager.mergePresets (/home/jpic/src/django-autocomplete-light/node_modules/babel-core/lib/transformation/file/options/option-manager.js:264:10)
        at OptionManager.mergeOptions (/home/jpic/src/django-autocomplete-light/node_modules/babel-core/lib/transformation/file/options/option-manager.js:249:14)
        at OptionManager.init (/home/jpic/src/django-autocomplete-light/node_modules/babel-core/lib/transformation/file/options/option-manager.js:368:12)
        at File.initOptions (/home/jpic/src/django-autocomplete-light/node_modules/babel-core/lib/transformation/file/index.js:212:65)
        at new File (/home/jpic/src/django-autocomplete-light/node_modules/babel-core/lib/transformation/file/index.js:135:24)
        at Pipeline.transform (/home/jpic/src/django-autocomplete-light/node_modules/babel-core/lib/transformation/pipeline.js:46:16)
        at transpile (/home/jpic/src/django-autocomplete-light/node_modules/babel-loader/lib/index.js:50:20)
        at Object.module.exports (/home/jpic/src/django-autocomplete-light/node_modules/babel-loader/lib/index.js:173:20)
     @ ./node_modules/select2/dist/js/select2.js 15:4-31
     @ ./src/dal/js/index.js
     @ multi babel-polyfill ./src/dal/js/index.js
    error Command failed with exit code 2.
    info Visit https://yarnpkg.com/en/docs/cli/add for documentation about this command.
    
    10/03 2018 01:07:04 jpic@jpic ~/src/django-autocomplete-light  (master)
    $ vim src/dal/js/index.js
    
    10/03 2018 01:07:52 jpic@jpic ~/src/django-autocomplete-light  (master)
    $ yarn prepare
    yarn run v1.3.2
    $ webpack
    Hash: 14db246c661da63a3077
    Version: webpack 3.11.0
    Time: 2920ms
          Asset    Size  Chunks                    Chunk Names
        dist.js  274 kB       0  [emitted]  [big]  main
    dist.js.map  556 kB       0  [emitted]         main
      [90] (webpack)/buildin/global.js 823 bytes {0} [built]
     [125] multi babel-polyfill ./src/dal/js/index.js 40 bytes {0} [built]
     [325] (webpack)/buildin/module.js 521 bytes {0} [built]
     [329] ./src/dal/js/index.js 157 bytes {0} [built]
        + 326 hidden modules
    Done in 3.25s.

---
+++ src/dal/js/index.js
@@ -0,0 +1,5 @@
+//:import * as select2 from 'select2'
+
+const dal = 'hello'
+
+export { dal }
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path')
+
+/* eslint-disable */
+module.exports = {
+/* eslint-enable */
+  entry: {
+    main: ['babel-polyfill', './src/dal/js/index.js'],
+  },
+  output: {
+    path: path.resolve('src/dal/static/dal'),
+    filename: 'dist.js'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        //exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['stage-2', 'babel-preset-env']
+          }
+        }
+      }
+    ]
+  }
+}
