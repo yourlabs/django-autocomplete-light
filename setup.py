b149815c212155df8778f12525f1efafac318e1b
jpic <jamespic@gmail.com>
2018-03-28 23:30:46 +0200

    Release 2.3.5

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ if 'sdist' in sys.argv:
 
 setup(
     name='django-autocomplete-light',
-    version='2.3.4',
+    version='2.3.5',
     description='Fresh autocompletes for Django',
     author='James Pic',
     author_email='jamespic@gmail.com',
