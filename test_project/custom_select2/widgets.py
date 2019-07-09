32e50a6b4fbb08fdab5eaa5ee3d5acfcdb1da1c3
jpic <jpic@yourlabs.org>
2019-07-09 17:08:16 +0200

    Python 2 support (mostly)

--- test_project/custom_select2/widgets.py
+++ test_project/custom_select2/widgets.py
@@ -8,6 +8,6 @@ class TModelSelect2(ModelSelect2):
 
     @property
     def media(self):
-        base_media = super().media
+        base_media = super(TModelSelect2, self).media
         custom_media = forms.Media(js=('t_select2.js',))
         return base_media + custom_media
