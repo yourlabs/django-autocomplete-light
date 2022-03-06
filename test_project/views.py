a945f118cce9e86dcbdc29190f0082426a465e8e
Bernd Wechner <bernd.wechner@gmail.com>
2022-03-06 11:10:33 +1100

    Two isolated unadorned simple test cases added
    Something sorely lacking from the test_project seems to be a really stripped down, isolated test of the single and multiselect widgets. Very useful and important I believe, for seeing the very bare minimum configuration for a working widget without a lot of very distracting dressing in templates and views. These a totally drab new pages to demonstrated the stripped down bare minimalist widgets in action.
    
    That said, something is wrong, and this first cut serves as little more than a minimal stripped won demonstration of the focus bug being discussed on github now . It is either a DAL bug or an incorrect use of DAL, but this tiny little view will help anyone assessing it to offer commentary on what has gone wrong. There's almost nothing there and hence it is, as per the goal, free of a lot of distracting context.

--- test_project/views.py
+++ test_project/views.py
@@ -4,10 +4,79 @@ except ImportError:  # old django, lol it
     from django.views.generic import TemplateView as LoginView
 from django.views import generic
 
+from django.urls import reverse_lazy
+from django.http.response import HttpResponse
+
+from dal import autocomplete
+from select2_many_to_many.models import TModel
+from django.forms.models import ModelChoiceIterator, ModelMultipleChoiceField
+
+
+class LoginView(LoginView):
+    template_name = 'login.html'
+
 
 class IndexView(generic.TemplateView):
     template_name = 'base.html'
 
 
-class LoginView(LoginView):
-    template_name = 'login.html'
+def BasicDALView(request):
+    '''
+    A very basic DAL widget alone on a page for minimalist testing. Not a dressed demo of what DAL can do,
+    rather an isolated functional widget page against which comparisons can be made if someone has a DAL
+    widget not working,
+
+    This is the basic DAL (single selected) widget.
+    '''
+    js = """
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.js" crossorigin="anonymous"></script>
+    """
+    # <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2-bootstrap-theme/0.1.0-beta.10/select2-bootstrap.min.css" integrity="sha512-kq3FES+RuuGoBW3a9R2ELYKRywUEQv0wvPTItv3DSGqjpbNtGWVdvT8qwdKkqvPzT93jp8tSF4+oN4IeTEIlQA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    # <script>$.fn.select2.defaults.set( "theme", "bootstrap" );</script>
+    # <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.full.js" crossorigin="anonymous"></script>
+
+    dal_media = autocomplete.Select2().media
+
+    url = reverse_lazy('select2_many_to_many_autocomplete')
+    field = ModelMultipleChoiceField(TModel.objects.all())
+
+    widget = autocomplete.ModelSelect2(url=url, attrs={"class": "selector", "id": id, "data-placeholder": "Placeholder"})
+    widget.choices = ModelChoiceIterator(field)
+
+    default = None
+    widget_html = widget.render(TModel.__name__, default)
+
+    html = f"<head>{js}\n{dal_media}</head><body><p>{widget_html}</p></body>"
+
+    return HttpResponse(html)
+
+
+def BasicDALMultiView(request):
+    '''
+    A very basic DAL widget alone on a page for minimalist testing. Not a dressed demo of what DAL can do,
+    rather an isolated functional widget page against which comparisons can be made if someone has a DAL
+    widget not working,
+
+    This is the multi select DAL widget.
+    '''
+    js = """
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.js" crossorigin="anonymous"></script>
+    """
+    # <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2-bootstrap-theme/0.1.0-beta.10/select2-bootstrap.min.css" integrity="sha512-kq3FES+RuuGoBW3a9R2ELYKRywUEQv0wvPTItv3DSGqjpbNtGWVdvT8qwdKkqvPzT93jp8tSF4+oN4IeTEIlQA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    # <script>$.fn.select2.defaults.set( "theme", "bootstrap" );</script>
+    # <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.full.js" crossorigin="anonymous"></script>
+
+    dal_media = autocomplete.Select2().media
+
+    url = reverse_lazy('select2_many_to_many_autocomplete')
+    field = ModelMultipleChoiceField(TModel.objects.all())
+
+    widget = autocomplete.ModelSelect2Multiple(url=url, attrs={"class": "selector", "id": id, "data-placeholder": "Placeholder"})
+    widget.choices = ModelChoiceIterator(field)
+
+    default = None
+    widget_html = widget.render(TModel.__name__, default)
+
+    html = f"<head>{js}\n{dal_media}</head><body><p>{widget_html}</p></body>"
+
+    return HttpResponse(html)
