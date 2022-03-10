feadfc9042b7dcefb6bcc0811ffc1e9ce4417d75
Bernd Wechner <bernd.wechner@gmail.com>
2022-03-10 22:05:14 +1100

    Fixed JQuery version so the test works!

--- test_project/views.py
+++ test_project/views.py
@@ -29,7 +29,7 @@ def BasicDALView(request):
     This is the basic DAL (single selected) widget.
     '''
     js = """
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.js" crossorigin="anonymous"></script>
     """
     # <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2-bootstrap-theme/0.1.0-beta.10/select2-bootstrap.min.css" integrity="sha512-kq3FES+RuuGoBW3a9R2ELYKRywUEQv0wvPTItv3DSGqjpbNtGWVdvT8qwdKkqvPzT93jp8tSF4+oN4IeTEIlQA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     # <script>$.fn.select2.defaults.set( "theme", "bootstrap" );</script>
