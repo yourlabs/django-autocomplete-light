025ed1dc0223965308ed1dec3fcb34bb754877a7
jpic <jpic@yourlabs.org>
2023-05-12 17:55:58 +0200

    Release 3.9.6rc1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name='django-autocomplete-light',
-    version='3.9.5',
+    version='3.9.6rc1',
     description='Fresh autocompletes for Django',
     author='James Pic',
     author_email='jamespic@gmail.com',
