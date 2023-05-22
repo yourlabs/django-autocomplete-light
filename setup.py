fda792de54b03baab977e99b5798bf65d1f2eed8
jpic <jpic@users.noreply.github.com>
2023-05-22 20:10:07 +0000

    Fixed ci

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name='django-autocomplete-light',
-    version='3.9.7',
+    version='3.9.8rc19',
     description='Fresh autocompletes for Django',
     author='James Pic',
     author_email='jamespic@gmail.com',
