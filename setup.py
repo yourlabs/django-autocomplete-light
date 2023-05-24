e3c919a8b8db76a9a890e9399245a47d66844824
jpic <jpic@users.noreply.github.com>
2023-05-24 09:05:06 +0200

    Release action: fix changelog execution

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name='django-autocomplete-light',
-    version='3.9.8rc24',
+    version='3.9.8rc29',
     description='Fresh autocompletes for Django',
     author='James Pic',
     author_email='jamespic@gmail.com',
